b9c428dcdcc557967b8eeddbe5ab2541df7f709f
iamkdeepesh <69793773+iamkdeepesh@users.noreply.github.com>
2022-02-28 23:50:14 +0530

    Add files via upload

---
+++ Assignment 5/4.java
@@ -0,0 +1,9 @@
+class JavaExample {
+   public static void main(String args[]) {
+	int n = 100;
+	System.out.print("Numbers from 1 to "+n+" are: ");
+	for (int i = n; i >=1; i--) {
+		System.out.print(i + " ");
+	}
+   }
+}
---
+++ Assignment 6/3.java
@@ -0,0 +1,14 @@
+public class PrintAsciiValueExample1   
+{  
+public static void main(String[] args)   
+{  
+// character whose ASCII value to be found  
+char ch1 = 'a';  
+char ch2 = 'b';  
+// variable that stores the integer value of the character  
+int asciivalue1 = ch1;  
+int asciivalue2 = ch2;  
+int num=asciivalue1+asciivalue2;
+System.out.println(num);  
+}  
+}  
